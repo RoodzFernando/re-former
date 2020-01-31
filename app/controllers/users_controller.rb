fb3bf34b2dfc481f2c137a8f7e62e3e20c174e8a
firmo holanda <firmo@viize.com>
2020-01-31 13:00:59 -0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   # def set_user @user = User.find(params[:id]) end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.permit(:username, :email, :password)
   end
 end
